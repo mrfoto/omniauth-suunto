39f855671460198800d5968e6faa2b5348a35221
Miha Rekar <info@mr.si>
2020-05-18 18:33:27 +0200

    0.0.2

--- lib/omniauth/omniauth-suunto.rb
+++ lib/omniauth/omniauth-suunto.rb
@@ -2,7 +2,7 @@ require 'omniauth/strategies/suunto'
 
 module Omniauth
   module Suunto
-    VERSION = '0.0.1'
+    VERSION = '0.0.2'
     
     class Error < StandardError; end
     # Your code goes here...
