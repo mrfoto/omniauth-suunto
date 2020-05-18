db0290c07124c81b851d11388b2df933bf107ba8
Miha Rekar <info@mr.si>
2020-05-18 16:00:41 +0200

    bundle gem

---
+++ lib/omniauth/suunto.rb
@@ -0,0 +1,8 @@
+require "omniauth/suunto/version"
+
+module Omniauth
+  module Suunto
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/suunto/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Suunto
+    VERSION = "0.1.0"
+  end
+end
